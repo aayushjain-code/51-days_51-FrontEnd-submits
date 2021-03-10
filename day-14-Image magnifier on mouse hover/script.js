ad6c41f26f8a168f333b1fd9d6295a71bb7eea84
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-03-10 19:31:30 +0530

    Add files via upload

---
+++ day-14-Image magnifier on mouse hover/script.js
@@ -0,0 +1,28 @@
+$(function () {
+  $(".xzoom, .xzoom-gallery").xzoom({
+    zoomWidth: 400,
+    tint: "#333",
+    Xoffset: 15,
+  });
+});
+   
+
+
+// function imageChange(){
+//   document.getElementsByClassName('xzoom').src = document.getElementsByClassName('xzoom-gallery').src;
+// }
+
+// $(document).ready(function(){
+//   $(".small_image").hover(function){
+//     $(".big_image").attr('src',$(this).attr('src')
+//     )};
+// });
+
+
+
+
+// $(document).ready(function(){
+//   $(".big_image").imagezoom({
+//     zoomrange:[3,3]
+//   });
+// });
